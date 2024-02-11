83a51e46ab639aeccff1501bfe5a018f3164e342
Diego Alexandro de Oliveira <diegoalexandrooliveira@gmail.com>
2024-02-11 09:23:11 -0300

    primeiro commit

---
+++ src/main/java/com/diegoalexandro/rinhabackend/RinhaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.diegoalexandro.rinhabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegoalexandro/rinhabackend/RinhaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoalexandro.rinhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
